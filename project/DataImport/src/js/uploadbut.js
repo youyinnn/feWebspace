b688f08ad19c659ac0062efdc391fc2dfff69e0a
youyinnn <454947049@qq.com>
2017-11-10 22:01:39 +0800

    文件上传卡顿无法解决

--- project/DataImport/src/js/uploadbut.js
+++ project/DataImport/src/js/uploadbut.js
@@ -1,26 +1,27 @@
-input = document.createElement('input')
+var input = document.createElement('input')
 input.setAttribute('id', 'uploadFile')
 input.setAttribute('type', 'file')
 input.setAttribute('name', 'myfile')
+input.setAttribute('value', null)
 input.style.display = 'none'
 
-/* global postReq xmlhttp */
+/* global postReq xmlhttp input msgPanelShow p2 */
 function uploadbut (maxSise, acceptableFileArr) {
   input.click()
-  input.value = null
   input.onchange = function () {
+    input.value = null
     console.log(input.files[0])
     if (!input.value) {
       return 0
     }
     if (maxSise && input.files[0].size > maxSise * 1024 * 1024) {
       console.log(input.files[0].size)
-      msgPanelShow(p2,'文件过大','请上传小于' + maxSise + 'M的文件！')
+      msgPanelShow(p2, '文件过大', '请上传小于' + maxSise + 'M的文件！')
       return 0
     }
     var fileType = input.value.split('.').pop()
-    if (acceptableFileArr.indexOf(fileType.toLocaleLowerCase()) == -1) {
-      msgPanelShow(p2,'文件过大','不接受的文件类型' + fileType)
+    if (acceptableFileArr.indexOf(fileType.toLocaleLowerCase()) === -1) {
+      msgPanelShow(p2, '文件过大', '不接受的文件类型' + fileType)
       return 0
     }
   }
